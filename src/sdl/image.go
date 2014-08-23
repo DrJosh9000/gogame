ad53a5a70f1a116237132fff87d9c5b167425a2c
Josh Deprez <josh@seikaku.local>
2014-08-23 11:46:49 +1000

    Image binding is officially TODO until ...sometime

--- src/sdl/image.go
+++ src/sdl/image.go
@@ -1,2 +1,14 @@
 package sdl
 
+/*
+#cgo CFLAGS: -I/Library/Frameworks/SDL2.framework/Headers -I/Library/Frameworks/SDL2_image.framework/Headers
+#cgo LDFLAGS: -F/Library/Frameworks -framework SDL2 -framework SDL2_image
+
+#include <stdlib.h>
+#include <SDL_image.h>
+*/
+//import "C"
+
+// Argh. The SDL_image.h assumes that SDL.h is in a directory called SDL2/ ...
+
+
