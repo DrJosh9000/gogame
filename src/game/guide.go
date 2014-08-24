ca9431434b8b2f5d6c528deb895d35242a39bf2f
Josh Deprez <josh@seikaku.local>
2014-08-24 22:40:14 +1000

    More guide

--- src/game/guide.go
+++ src/game/guide.go
@@ -6,6 +6,13 @@ import (
 	"sdl"
 )
 
+const (
+	keyGuideFrameWidth, keyGuideFrameHeight = 64, 64
+	keyGuideUpdateInterval = 500 * time.Millisecond
+	keyGuideTextureFile = "assets/keys.png"
+	
+)
+
 type KeyGuide int
 const (
 	KeyGuideA KeyGuide = iota
@@ -15,14 +22,37 @@ const (
 
 type Guide struct {
 	tex *sdl.Texture
-	x, y int
+	x, y, frame int
 	KeyGuide
 	
 	Updater *time.Ticker
 }
 
 func NewGuide(ctx *sdl.Context, k KeyGuide) (*Guide, error) {
+	tex, err := ctx.GetTexture(keyGuideTextureFile)
+	if err != nil {
+		return nil, err
+	}
 	
-	return nil, nil
+	g := &Guide{
+		tex: tex,
+		KeyGuide: k,
+		Updater: time.NewTicker(keyGuideUpdateInterval),
+	}
+	go g.life()
+	return g, nil
 }
 
+func (g *Guide) Draw(r *sdl.Renderer) error {
+	return r.Copy(g.tex, sdl.Rect(g.frame*keyGuideFrameWidth, int(g.KeyGuide)*keyGuideFrameHeight, keyGuideFrameWidth, keyGuideFrameHeight), sdl.Rect(g.x, g.y, keyGuideFrameWidth, keyGuideFrameHeight))
+}
+
+func (g *Guide) Destroy() {
+	g.Updater.Stop()
+}
+
+func (g *Guide) life() {
+	for _ = range g.Updater.C {
+		g.frame = (g.frame+1)%2
+	}
+}
