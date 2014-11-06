878a84e7023549909f1b59f059f2b08e2b0f4305
Josh Deprez <josh@seikaku.local>
2014-11-06 20:18:36 +1100

    started commenting my code

--- src/game/notify.go
+++ src/game/notify.go
@@ -1,2 +1,22 @@
 package game
 
+// message is the interface for notification messages.
+type message interface {
+	String() string
+}
+
+// notes keeps track of all registered channels.
+var notes = map[string][](chan message) {}
+
+// kmp stands for "keep me posted", and registers a callback channel
+// for messages sent to a given key.
+func kmp(key string, ch chan message) {
+	notes[key] = append(notes[key], ch)
+}
+
+// notify sends a message to every channel registered for a key.
+func notify(key string, m message) {
+	for _, n := range notes[key] {
+		n <- m
+	}
+}
