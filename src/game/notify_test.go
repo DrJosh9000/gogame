3da4ab4e8be4da87ebeafc4b2e7a1410174982be
Josh Deprez <josh@seikaku.local>
2014-11-08 08:34:47 +1100

    More test

--- src/game/notify_test.go
+++ src/game/notify_test.go
@@ -10,8 +10,18 @@ func (t testMessage) String() string {
 	return fmt.Sprintf("%d", int(t))
 }
 
+func TestRegister(t *testing.T) {
+	kmp("TestRegister", nil)
+	if got, want := len(notes["TestRegister"]), 1; got != want {
+		t.Fatalf("Got len(notes[TestRegister]) = %v, want $v", got, want)
+	}
+	if got := notes["TestRegister"][0]; got != nil {
+		t.Fatalf("Got notes[TestRegister][0] = %v, want nil", got)
+	}
+}
+
 func TestNotify(t *testing.T) {
-	c := make(chan message)
+	c := make(chan message, 10)
 	kmp("TestNotify", c)
 	go notify("TestNotify", testMessage(5))
 	m, ok := <-c
