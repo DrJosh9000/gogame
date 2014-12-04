c465cd6d562f6376366c90b193179d138d103594
Josh Deprez <josh@seikaku.local>
2014-12-04 19:37:39 +1100

    Menu system start

--- src/game/menu.go
+++ src/game/menu.go
@@ -1,3 +1,15 @@
 package game
 
 // TODO: menu system goes here
+
+type menuAction func() error
+var menus = map[string]menuAction {
+	"Start game": func() error {
+		// TODO: start a game
+		return nil
+	},
+	"Quit": func() error {
+		notify("global", quitMsg)
+		return nil
+	},
+}
